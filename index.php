43b039d1242e50a35e9f59fd2e4579532841bf13
Peppino Impastato <alegay@gmail.com>
2024-03-26 22:19:24 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<head>
+  <title>maro marciano</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+
+<body>
+    <h1>Il mio curriculum</h1>
+    <img class="rosso" id="marciano" src="./Media/ilmarzone.png">
+    <img class="rosso blur" id="jett" src="./Media/jett.jpg">
+
+
+    <p>Benvenuto sul mio curriculum</p>
+    <p><a target="_blank" href="https://www.instagram.com/dreamnailing/?hl=it">Clicca qui</a> per accedere al mio
+        profilo ig</p>
+
+    <style>
+        img {
+            border-radius: 50%;
+            width: 410px;
+        }
+
+        #marciano {
+            width: 80px;
+            border-radius: 10%;
+        }
+
+        .rosso {
+            border: 1px solid red;
+        }
+    </style>
+</body>
